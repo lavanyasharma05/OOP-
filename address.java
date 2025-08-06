a8a47c5bd19856b097e450f8eac36a18505f8733
lavanyasharma05 <lavanyasharma1105@gmail.com>
2025-08-06 16:48:17 +0530

    java

---
+++ address.java
@@ -0,0 +1,8 @@
+public class address {
+
+public static void main(String[] args) { 
+  System.out.println("Paschim Vihar"); 
+  System.out.println("New Delhi") 
+  System.out.print("Lavany Sharma\n");
+
+}
