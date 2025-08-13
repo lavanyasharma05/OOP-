aa8124903cbf1dd7b306ead62075ee9ea283c6ff
lavanyasharma05 <lavanyasharma1105@gmail.com>
2025-08-13 16:06:09 +0530

    java

---
+++ Swap.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class SwapValues {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first positive integer (a): ");
+        int a = scanner.nextInt();
+
+        System.out.print("Enter second positive integer (b): ");
+        int b = scanner.nextInt();
+
+        System.out.println("\nSwapping using a third variable");
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("After swap: a = " + a + ", b = " + b);
+
+        
+    }
+}
