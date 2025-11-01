073de406dc370fce39540005ef13d5feea06e4c7
lavanyasharma05 <lavanyasharma1105@gmail.com>
2025-11-01 15:03:49 +0530

    .java

---
+++ Geek.java
@@ -0,0 +1,8 @@
+import java.io.*;
+class Geeks {
+    public static void main(String[] args) {
+String s = "lavanya";
+System.out.println(s.toUpperCase()); 
+System.out.println(s.toLowerCase());
+    }
+}
